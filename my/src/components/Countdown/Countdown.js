e5474f34268be66b135590e9b8c69d0b06158c4f
Your Name <you@example.com>
2021-03-29 15:33:45 +0600

    fixed component countdown in folder Countdown

--- my/src/components/Countdown/Countdown.js
+++ my/src/components/Countdown/Countdown.js
@@ -1,19 +1,15 @@
-import classes from "./Countdown.module.css";
 import { useEffect } from "react";
-
-const Countdown = ({ countdown, setCountdown}) => {
-    useEffect(function() {
-        if (!countdown) return;
-
-        const interval = setInterval(() => {
-            setCountdown(countdown = 1)
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [countdown]);
-    return ( 
+import classes from "./Countdown.module.css";
+const Countdown = ({ countdown, setCountdown }) => {
+  useEffect(() => {
+    if (!countdown) return;
+    const interval = setInterval(function() {
+      setCountdown(countdown - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [countdown]);
+  return (
     <div className={classes.Countdown}>{countdown}</div>
-     );
+  );
 }
- 
 export default Countdown;
