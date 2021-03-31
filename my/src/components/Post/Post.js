dbfa167c39bf87abda92dce3088d6735eab9e31f
Your Name <you@example.com>
2021-03-31 14:57:59 +0600

    create post components in folder src

---
+++ my/src/components/Post/Post.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Posts from './components/Posts/Posts';
+function App() {
+  const [message, setMessage] = useState("Hello");
+  console.log("[App.js] render");
+  return (
+    <div className="App">
+      {message}
+      <button onClick={() => setMessage("Bye")}>Toggle</button>
+      <Posts message={message} />
+    </div>
+  );
+}
+export default App;
