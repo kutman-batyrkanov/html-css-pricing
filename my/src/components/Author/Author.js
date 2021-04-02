52121675755a8dbe8a692fcfab0aadeec567ce50
Your Name <you@example.com>
2021-04-02 15:53:43 +0600

    fixed component author

--- my/src/components/Author/Author.js
+++ my/src/components/Author/Author.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import classes from "./Author.module.css";
 
-const Author = ({  }) => {
+const Author = ({ userid }) => {
+    const [email, setEmail] = useState("Loading");
+
+    
     return ( 
-    <div className={classes.Author}></div>
-     );
+        <div className = {classes.Author}>
+            {userid}
+        </div>
+    );
 }
- 
+
 export default Author;
