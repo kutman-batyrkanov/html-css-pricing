309c0520e0a7f1aedc149ea9c9ee331e7155583e
Your Name <you@example.com>
2021-04-20 22:45:14 +0600

    redux example proect

---
+++ redux-example/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CounterDisplay from './components/CounterDisplay';
+import CounterSimpleControls from './components/CounterSimpleControls';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterDisplay />
+      <CounterSimpleControls />
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-example/src/components/CounterDisplay.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+
+const CounterDisplay = () => {
+    const number = useSelector(state => state.number);
+
+    return ( 
+    <h1>{number}</h1>
+     );
+}
+ 
+export default CounterDisplay;
