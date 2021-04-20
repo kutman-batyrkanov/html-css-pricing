3ad79d664c1b0ddcc2cf6d6ca3176e613f9e9d4d
Your Name <you@example.com>
2021-04-20 23:42:23 +0600

    add and use ddecrement by 10 in component redusers

--- redux-example/src/redux/redusers.js
+++ redux-example/src/redux/redusers.js
@@ -8,8 +8,10 @@ const counterReducer = (state = initialState, ation) => {
           return { ...state, number: state.number + 1};
         case "decrement_by_1":
             return { ...state, number: state.number - 1}; 
-        case "icrement_by_10":
-            return { ...state, number: state.number + 10};   
+        case "increment_by_10":
+            return { ...state, number: state.number + 10};  
+        case "decrement_by_10":
+            return { ...state, number: state.number - 10};    
     }
 
     return state;
